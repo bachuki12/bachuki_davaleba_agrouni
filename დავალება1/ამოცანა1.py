3e06f66e2c5a07cc47b38be2d29413d7020ace7e
Your Name <you@example.com>
2025-10-17 14:54:57 +0400

    დავალება

--- დავალება1/ამოცანა1.py
+++ დავალება1/ამოცანა1.py
@@ -1,14 +1,30 @@
 
 
 #1) დაწერეთ პროგრამა, რომელიც მომხმარებელს მოსთხოვს სამკუთხედის გვერდების შეყვანას და ეკრანზე დაბეჭდავს მის ფართობს და პერიმეტრს.
+import math
 
 side_list = [0,0,0] # ვქმნით ლისტს სამკუთხედისთვის
 
-for i in range(len(side_list)):
-    gvedri = int(input(f"შემოიტანეთ სამკუთხედის გვერდი {i+1} სიგრძე ")) # მომხმარებლისგან ვიღებთ გვერდების სიგრძეს
-    side_list[i] = gvedri # ლისტში ვამატებთ ელემენტებს
-    perimetri =sum(side_list) # ვჯამავთ ლისტის ელემენტებს
-    samkutxedis_fartobi =  perimetri/2  #სამკუთხედის ფართობი გამოითვლება ფორმულით პრიმეტრი/2
+cheshmariti =True
 
-print(f"სამკუთხედის პერიმეტრია ტოლია {perimetri}-სმ") #უბრალოდ ვბეჭდავთ სამკუთხედის პერიმეტრის მნიშვნელობას
-print(f"სამკუთხედის ფართობი ტოლია {samkutxedis_fartobi}-სმ^2")  #უბრალოდ ვბეჭდავთ სამკუთხედის ფათობის მნიშვნელობას
+
+for i in range(len(side_list)):
+    gvedri = int(input(f"შემოიტანეთ სამკუთხედის გვერდის სიგრძე "))  # მომხმარებლისგან ვიღებთ გვერდების სიგრძეს
+    if gvedri<=0:
+        break
+    else:
+        side_list[i] = gvedri
+
+gverdi_1_plus_2 = side_list[0]+side_list[1]
+gverdi_1_plus_3 = side_list[0]+side_list[2]
+gverdi_2_plus_3 = side_list[1]+side_list[2]
+
+if gverdi_1_plus_2 <= side_list[2] or gverdi_2_plus_3 <= side_list[0] or gverdi_1_plus_3 <= side_list[1]:
+    print("ასეთი გვერდების მქონე სამკუთხედი არ არსებობს ")
+else:
+    perimetri =sum(side_list) # ვაჯამავთ ლისტის ელემენტებს
+    s =  perimetri/2  #სამკუთხედის ფართობი გამოითვლება ფორმულით პრიმეტრი/2
+    area =math.sqrt(s*((s-side_list[0])*(s-side_list[1])*(s-side_list[2])))
+
+    print(f"სამკუთხედის პერიმეტრია ტოლია {perimetri}-სმ") #უბრალოდ ვბეჭდავთ სამკუთხედის პერიმეტრის მნიშვნელობას
+    print(f"სამკუთხედის ფართობი ტოლია {area}-სმ²")  #უბრალოდ ვბეჭდავთ სამკუთხედის ფათობის მნიშვნელობას
