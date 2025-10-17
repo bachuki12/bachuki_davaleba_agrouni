b3963d71d176e3d33da719bb3168760e7b964a80
Your Name <you@example.com>
2025-10-17 14:59:50 +0400

    დავალება

--- დავალება1/ამოცანა1.py
+++ დავალება1/ამოცანა1.py
@@ -1,30 +1,27 @@
 
 
 #1) დაწერეთ პროგრამა, რომელიც მომხმარებელს მოსთხოვს სამკუთხედის გვერდების შეყვანას და ეკრანზე დაბეჭდავს მის ფართობს და პერიმეტრს.
-import math
+import math # ბიბლიოთეკაა მათემატიკური ოპერაციებისთვის მაგალიტად ფესვის ამოღება  math.sqrt
 
 side_list = [0,0,0] # ვქმნით ლისტს სამკუთხედისთვის
 
-cheshmariti =True
-
-
 for i in range(len(side_list)):
     gvedri = int(input(f"შემოიტანეთ სამკუთხედის გვერდის სიგრძე "))  # მომხმარებლისგან ვიღებთ გვერდების სიგრძეს
-    if gvedri<=0:
+    if gvedri<=0: # ვამოიწმებ მომხმარებეკლმა უაყოფითი ნიშნის მქონე რიცხვი ხომ არ შემოიტანა
         break
     else:
-        side_list[i] = gvedri
+        side_list[i] = gvedri # ლისტსი ვწერე მომხამრებლის მიერე შემოტანილ მონაცემებს
 
 gverdi_1_plus_2 = side_list[0]+side_list[1]
 gverdi_1_plus_3 = side_list[0]+side_list[2]
 gverdi_2_plus_3 = side_list[1]+side_list[2]
 
-if gverdi_1_plus_2 <= side_list[2] or gverdi_2_plus_3 <= side_list[0] or gverdi_1_plus_3 <= side_list[1]:
+if gverdi_1_plus_2 <= side_list[2] or gverdi_2_plus_3 <= side_list[0] or gverdi_1_plus_3 <= side_list[1]: # ვიყენებ სამკუთხედის წეს
     print("ასეთი გვერდების მქონე სამკუთხედი არ არსებობს ")
 else:
     perimetri =sum(side_list) # ვაჯამავთ ლისტის ელემენტებს
-    s =  perimetri/2  #სამკუთხედის ფართობი გამოითვლება ფორმულით პრიმეტრი/2
-    area =math.sqrt(s*((s-side_list[0])*(s-side_list[1])*(s-side_list[2])))
+    s =  perimetri/2  #სამკუთხედის პერიმეტრის ნახევარს ვითვლი
+    area =math.sqrt(s*((s-side_list[0])*(s-side_list[1])*(s-side_list[2]))) # ვითვლი სამკუთხედის ფართობს
 
     print(f"სამკუთხედის პერიმეტრია ტოლია {perimetri}-სმ") #უბრალოდ ვბეჭდავთ სამკუთხედის პერიმეტრის მნიშვნელობას
     print(f"სამკუთხედის ფართობი ტოლია {area}-სმ²")  #უბრალოდ ვბეჭდავთ სამკუთხედის ფათობის მნიშვნელობას
