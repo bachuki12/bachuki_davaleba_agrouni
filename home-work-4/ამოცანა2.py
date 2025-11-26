ba3737efd5c723a27e392632fe20256fde2180f4
acer <bachuki.bakuradze.1@iliauni.edu.ge>
2025-11-26 23:58:59 +0400

    დავალება4

---
+++ home-work-4/ამოცანა2.py
@@ -0,0 +1,48 @@
+'''
+დაწერეთ ფუნქცია, რომელიც მიიღებს ორ ნატურალურ რიცხვს 10000-მდე და ეკრანზე გამოიტანს მათ უდიდეს საერთო გამყოფს - GCD.
+
+მაგალითი 1:
+Enter a: 1000
+Enter b: 400
+GCD of 1000 and 400 is 200.
+
+მაგალითი 2:
+Enter a: 10000
+Enter b: 17
+GCD of 10000 and 17 is 1.
+
+'''
+tmp = 1
+
+a = input("Enter number1: ")
+b = input("Enter number2: ")
+
+def gcd(a, b):
+    copy_of_b = b
+    copy_of_a = a
+    while (b != 0):
+            tmp = a % b
+            a = b
+            b = tmp
+
+    print(f"GCD of {copy_of_a} and {copy_of_b} is {a}. ")
+
+while(True):
+    if a.isdigit() and b.isdigit():
+        a = int(a)
+        b = int(b)
+        if a < 10000 and b < 10000:
+            gcd(a, b)
+            break
+        else:
+            print("Input values Out of range")
+            a = str(a)
+            b = str(b)
+            a = input("Enter correct number1: ")
+            b = input("Enter correct number2: ")
+    else:
+        print("Input value is negative or it's not numeric :(")
+        a = input("Please Enter correct number1: ")
+        b = input("Please Enter correct number2: ")
+
+
---
+++ home-work-4/ამოცანა3.py
@@ -0,0 +1,60 @@
+'''
+
+დაწერეთ ფუნქცია, რომელიც მიიღებს ორ ნატურალურ რიცხვს
+10000-მდე და ეკრანზე გამოიტანს მათ უმცირეს საერთო ჯერადს - LCM.
+იმპორტი გაუკეთეთ საერთო გამყოფის ფუნქციას წინა მაგალითიდან და მისი საშუალებით
+დაათვლევინეთ უმცირესი საერთო ჯერადი.
+მინიშნება: lcm(a, b) = a * b / gcd(a, b).
+
+მაგალითი 1:
+Enter a: 1000
+Enter b: 400
+LCM of 1000 and 400 is 2000.
+
+მაგალითი 2:
+Enter a: 500
+Enter b: 50
+LCM of 500 and 50 is 500.
+
+ფუნქციის ქვევით გამოიძახეთ ის რამდენიმეჯერ და დარწმუნდით მის სისწორეში.
+
+'''
+
+
+tmp = 1
+
+a = input("Enter number1: ")
+b = input("Enter number2: ")
+copy_of_b = b
+copy_of_a = a
+def gcd(a, b):
+    while (b != 0):
+            tmp = a % b
+            a = b
+            b = tmp
+
+    return a
+
+while(True):
+    if a.isdigit() and b.isdigit():
+        a = int(a)
+        b = int(b)
+        if a < 10000 and b < 10000:
+            print(gcd(a, b))
+            break
+        else:
+            print("Input values Out of range")
+            a = str(a)
+            b = str(b)
+            a = input("Enter correct number1: ")
+            b = input("Enter correct number2: ")
+    else:
+        print("Input value is negative or it's not numeric :(")
+        a = input("Please Enter correct number1: ")
+        b = input("Please Enter correct number2: ")
+
+def lcm(a, b):
+    val_lcm = int(a * b / gcd(a, b))
+    return  f"LCM of {copy_of_a} and {copy_of_b} is {val_lcm}."
+
+print(lcm(a, b))
